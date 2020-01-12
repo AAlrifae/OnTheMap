45aa1f7aa0f2cce1a276959f106a46bc20851404
AAlrifae <a.a.alrifae@gmail.com>
2020-01-12 22:17:33 +0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Abdulrahman Alrifae on 1/12/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
